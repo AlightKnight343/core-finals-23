747a3bd4f7bbfa658d7494720f6321e4d5aad070
gamer-1478 <aayushgarg2006@gmail.com>
2023-10-14 00:43:16 +0530

    fixed ui and functions of reg and login

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,59 +6,53 @@ const User = require('../schemas/userSchema.js'),
   passport = require('passport'),
   { ensureAuthenticated, forwardAuthenticated } = require('../middleware/authenticate.js');
 
-  //register
+//register
 router.get('/register', forwardAuthenticated, (req, res) => {
-  res.render('auth/register', {user: req.user, messages:req.flash()})
+  res.render('auth/register', { user: req.user, messages: req.flash() })
 })
 
 
 router.post('/register', async (req, res) => {
   let errors = [];
-  const { name, password, confirmPassword, email } = req.body;
+  const { name, password, email } = req.body;
 
   if (!name || !password || !email) {
     errors.push({ msg: "All fields are required" })
   };
-  if (password != confirmPassword) {
-    errors.push({ msg: "Passwords do not match" });
-  }
+  User.findOne({ email: email }).then((user) => {
+    if (user) {
+      errors.push('error', 'User already exists, try logging in instead.')
+    }
+  })
   if (errors.length > 0) {
     res.send(errors);
-  } else {
-
-    User.findOne({ email:email }).then((user) => {
-      if (user) {
-        req.flash('error', 'User already exists, try logging in instead.')
-        return res.redirect('/auth/register')
-      }
-      const userId = uuid();
-      const newUser = new User({
-        email:email,
-        name: name,
-        password: password,
-        userId: userId,
-      });
-      bcrypt.genSalt(10, (err, salt) =>
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser.save().then((user) => {
-            passport.authenticate('local', (err, user, info) => {
-              if (err) throw err;
-              if (!user) res.send({ "msg": `${info.message}` });
-              else {
-                req.logIn(user, (err) => {
-                  if (err) throw err;
-                  res.render('auth/login', { msg: "Successfully Authenticated", success: "true", user: req.user });
-                });
-              }
-            })(req, res);
-
-          }).catch((err) => console.log(err));
-        })
-      );
-    });
   }
+  const userId = uuid();
+  const newUser = new User({
+    email: email,
+    name: name,
+    password: password,
+    userId: userId,
+  });
+  bcrypt.genSalt(10, (err, salt) =>
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser.save().then((user) => {
+        passport.authenticate('local', (err, user, info) => {
+          if (err) throw err;
+          if (!user) res.send({ "msg": `${info.message}` });
+          else {
+            req.logIn(user, (err) => {
+              if (err) throw err;
+              res.send([{ msg: "Successfully Authenticated", success: "true", user: req.user }]);
+            });
+          }
+        })(req, res);
+
+      }).catch((err) => console.log(err));
+    })
+  );
 
 })
 
@@ -66,7 +60,7 @@ router.post('/register', async (req, res) => {
 //login 
 
 router.get('/login', forwardAuthenticated, (req, res) => {
-  res.render('auth/login', {user: req.user})
+  res.render('auth/login', { user: req.user })
 })
 
 router.post('/login', async (req, res, next) => {
@@ -78,7 +72,7 @@ router.post('/login', async (req, res, next) => {
     } else {
       req.logIn(user, (err) => {
         if (err) throw err;
-        res.redirect('/dashboard');
+        res.send([{success: "true", user: req.user}])
       });
     }
   })(req, res, next);
