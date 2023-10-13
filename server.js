6b28da6b0e1162d508ae9ce1cd332e44849baa97
gamer-1478 <aayushgarg2006@gmail.com>
2023-10-13 18:52:35 +0530

    auth routes

--- server.js
+++ server.js
@@ -5,58 +5,32 @@ const session = require('cookie-session');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const cookieParser = require("cookie-parser");
-const ejs  = require('ejs');
-// const ejsLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
+const ejsLayouts = require('express-ejs-layouts');
 const cors = require('cors');
 const passportInit = require('./middleware/passport.js')
 var server = require('http').createServer(app);
 const flash = require('express-flash')
 
 //file imports
-
-if (process.env.NODE_ENV === 'production') {
-    app.enable('trust proxy');
-}
-else {
-    app.disable('trust proxy');
-}
-
-
-//cors middleware
-const corsOptions = {
-    origin: (origin, callback) => {
-        callback(null, true);
-    },
-    credentials: true
-}
-app.use(cors(corsOptions))
+const authRoutes = require('./routers/authRoute.js')
 
 //middlewares
 app.use(express.json({ limit: '50mb' }), express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(express.static('public'))
 app.use(flash())
-// app.use(ejsLayouts)
+app.use(ejsLayouts)
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
-if (process.env.NODE_ENV === 'production') {
-    app.use(session({
-        secret: process.env.SESSION_SECRET,
-        resave: true,
-        saveUninitialized: true,
-        sameSite: 'none',
-        overwrite: true,
-        secure: true,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }));
-} else {
-    app.use(session({
-        secret: process.env.SESSION_SECRET,
-        resave: true,
-        saveUninitialized: true,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }));
-} 
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+}));
+
 app.use(cookieParser(process.env.SESSION_SECRET));
 
 passportInit(passport)
@@ -69,6 +43,7 @@ const dbUri = process.env.MONGO_URI
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("Connected to mongodb"))
 
 //routing
+app.use('/auth', authRoutes)
 
 //listen
 const PORT = process.env.PORT || 5000
